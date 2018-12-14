469c84ee7226bb1348dc0ca975a07dd219015b7a
Gemius01 <pablo.gemio@inacapmail.cl>
2018-12-13 23:25:59 -0300

    nuevo excel

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Exports\PruebasExport;
-use Maatwebsite\Excel\Facades\Excel;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Box\Spout\Writer\WriterFactory;
+use Box\Spout\Common\Type;
+use Illuminate\Support\Facades\DB;
 
 class ReporteController extends Controller
 {
@@ -17,17 +19,24 @@ class ReporteController extends Controller
     {
         //dd($request->all());
         ini_set('memory_limit',-1);
-        $fecha_inicio = $request->input('fecha_inicio');
-        $fecha_termino = $request->input('fecha_termino');
-        ///setlocale(LC_ALL,"es_ES");
-        //$string = "24/11/2014";
-        
-        //$fechaIn = Carbon::parse($fecha_inicio.'00:00:00')->format('F');
-        //dd(strftime( '%B' [, int $fechaIn = time() ] ));
-        //dd($fechaIn);
-        
-        //dd(strftime("%V,%G,%Y", strtotime("12/28/2002")))
-        return (new PruebasExport($fecha_inicio, $fecha_termino))->download('invoices.xlsx');
-        //return Excel::download(new PruebasExport($fecha_inicio, $fecha_termino), 'Reporte_Tráfico__2018.xlsx');
+        set_time_limit(0);
+        $fechaIn = Carbon::parse($request->input('fecha_inicio').'00:00:00');
+        $fechaTer = Carbon::parse($request->input('fecha_termino').'23:59:59');
+        $users = DB::select(DB::raw("select * FROM (SELECT username, DATE_FORMAT(acctstarttime, '%d-%m-%Y') as acctstarttime,DATE_FORMAT(acctstarttime, '%H:%i:%s') as horatstarttime, DATE_FORMAT(acctstoptime, '%d-%m-%Y') as acctstoptime, DATE_FORMAT(acctstoptime, '%H:%i:%s') as horaacctstoptime,round(acctinputoctets/1000000, 1) as acctinputoctets, round(acctoutputoctets/1000000, 1) as acctoutputoctets,  round(acctsessiontime/60, 1) AS session FROM radiusdb.radacct WHERE acctstarttime BETWEEN '".$fechaIn."' AND '".$fechaTer."') AS fecha, (SELECT mac, SUBSTRING_INDEX(SUBSTRING_INDEX(ip,'.',-2),'.',1) as ipmac, CASE WHEN destino = '' THEN 'http://www.google.cl/' ELSE destino END AS destino, case  when system like '%Android%' then 'Smarthphone' when  system like '%Apple%' then 'Smarthphone' else 'PC' end as type FROM radiusrepodb.maclist GROUP BY mac) AS mac, (select id, zona, localidad, comuna, servicio from radiusrepodb.zonaswifi) as zonaswifi WHERE (fecha.username=mac.mac and zonaswifi.id = mac.ipmac)"));
+             
+            $writer = WriterFactory::create(Type::XLSX);
+            
+            $writer->openToBrowser('Reporte.xlsx');
+            
+            $writer->addRow(['Rut Empresa', 'DV', 'Código de la Empresa STI', 'Región', 'Comuna', 'Localidad', 'Código Denomicación AP', 'Fecha inicio servicio', 'Nro Servicio', 'Fecha inicio Conexión', 'Hora Inicio Conexión', 'Fecha Termino Conexión', 'Hora Termino Conexión', 'Usuario (MAC) Conectado', 'Tráfico UP (en Mbps)', 'Tráfico down (en Mbps)', 'Uptime (minutos)', 'Uptime (minutos)', 'Tipo dispositivo (PC/Tablet/Smartphone)', 'Primera URL de navegación' ]);
+         
+            foreach($users as $user)
+            {
+
+                $writer->addRow([96884450, 4,420,7,$user->comuna,$user->localidad,$user->zona, $user->servicio,$user->id,$user->acctstarttime,$user->horatstarttime, $user->acctstoptime,$user->horaacctstoptime, $user->username, $user->acctinputoctets, $user->session, $user->type, $user->destino]);
+           
+            }
+            $writer->close();
+            return 'asd';
     }
 }
